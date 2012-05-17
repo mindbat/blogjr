fc44e382f224e76d3e6f3f7f5065f6f936041385
Ron Toland <rontoland@gmail.com>
2012-05-17 07:50:02 -0700

    Re-arrange the parens so db.clj and view.clj are more idiomatic

--- src/blogjr/db.clj
+++ src/blogjr/db.clj
@@ -1,15 +1,12 @@
 (ns blogjr.db
-  (:use clojure.contrib.sql)
-)
+  (:use clojure.contrib.sql))
 
 (def *db*
   {:subprotocol "postgresql" 
    :subname "//localhost:5432/blogjr"
    :username "blogjr"
    :password ""
-   :classname "org.postgresql.jdbcDriver"
-  }
-)
+   :classname "org.postgresql.jdbcDriver"})
 
 (defn create-posts []
   (with-connection *db*
@@ -17,17 +14,11 @@
       [:id "SERIAL" "PRIMARY KEY"]
       [:title :varchar "NOT NULL"]
       [:body :varchar "NOT NULL"]
-      [:created_at :timestamp]
-    )
-  )
-)
+      [:created_at :timestamp])))
 
 (defn now [] 
   (java.sql.Timestamp. 
-    (.getTime (java.util.Date.)
-    )
-  )
-)
+    (.getTime (java.util.Date.))))
 
 (defn insert-sample-posts []
   (let [timestamp (now)]
@@ -36,26 +27,15 @@
         (insert-values :posts
           [:title :body :created_at]
           ["First Post" "This is your first post." timestamp]
-          ["Second Post" "Your second post is longer than the first." timestamp]
-        )
-      )
-    )
-  )
-)
+          ["Second Post" "Your second post is longer than the first." timestamp])))))
 
 (defn select-posts []
   (seq
     (with-connection *db*
-      (with-query-results res ["select * from posts order by id desc"] (doall res)
-      )
-    )
-  )
-)
+      (with-query-results res ["select * from posts order by id desc"] (doall res)))))
 
 (defn sql-query [q]
-  (with-query-results res q (doall res)
-  )
-)
+  (with-query-results res q (doall res)))
 
 (defn last-created-id
   "Extract the last created id. Must be called in a transaction
@@ -65,42 +45,27 @@
   (first
     (vals
       (first 
-        (sql-query ["SELECT lastval();"])
-      )
-    )
-  )
-)
+        (sql-query ["SELECT lastval();"])))))
 
 (defn insert-post [title body]
   (with-connection *db*
     (transaction
       (insert-values :posts
         [:title :body :created_at]
-        [title body (now)]
-      )
-      (last-created-id)
-    )
-  )
-)
+        [title body (now)])
+      (last-created-id))))
 
 (defn post-update [id title body]
   (with-connection *db*
     (transaction
       (update-values :posts
         ["id=?" id]
-        {:title title :body body}
-      )
-    )
-  )
-)
+        {:title title :body body}))))
 
 (defn select-post [id]
   (first
     (with-connection *db*
-      (sql-query [(str "select * from posts where id = " id)])
-    )
-  )
-)
+      (sql-query [(str "select * from posts where id = " id)]))))
 
 (defn create-users []
   (with-connection *db*
@@ -109,41 +74,27 @@
       [:name :varchar "NOT NULL"]
       [:pass :varchar "NOT NULL"]
       [:created_at :timestamp]
-      [:last_login :timestamp]
-    )
-  )
-)
+      [:last_login :timestamp])))
 
 (defn insert-user [name pass]
   (with-connection *db*
     (transaction
       (insert-values :users
         [:name :pass :created_at]
-        [name pass (now)]
-      )
-      (last-created-id)
-    )
-  )
-)
+        [name pass (now)])
+      (last-created-id))))
 
 (defn is-user? [name pass]
   (let [id 
     (first
       (with-connection *db*
-        (sql-query [(str "select id from users where name = '" name "' and pass = '" pass "'")])
-      )
-    )]
-    (if id true false)
-  )
-)
+        (sql-query [(str "select id from users where name = '" name "' and pass = '" pass "'")])))]
+    (if id true false)))
 
 (defn delete-post [id]
   (with-connection *db*
     (transaction
-      (delete-rows :posts ["id=?" id])  
-    )
-  )
-)
+      (delete-rows :posts ["id=?" id]))))
 
 (defn create-comments []
   (with-connection *db*
@@ -153,25 +104,16 @@
       [:body :varchar "NOT NULL"]
       [:created_at :timestamp]
       [:author :varchar "NOT NULL"]
-      ["FOREIGN KEY" "(post_id)" "REFERENCES" :posts "(id)"]
-    )
-  )
-)
+      ["FOREIGN KEY" "(post_id)" "REFERENCES" :posts "(id)"])))
 
 (defn insert-comment [post_id body author]
   (with-connection *db*
     (transaction
       (insert-values :comments
         [:post_id :body :author :created_at]
-        [post_id body author (now)]
-      )
-      (last-created-id)
-    )
-  )
-)
+        [post_id body author (now)])
+      (last-created-id))))
 
 (defn select-comments [post_id]
   (with-connection *db*
-    (sql-query [(str "select * from comments where post_id = " post_id)])
-  )
-)
+    (sql-query [(str "select * from comments where post_id = " post_id)])))
--- src/blogjr/view.clj
+++ src/blogjr/view.clj
@@ -1,67 +1,36 @@
 (ns blogjr.view
-  (:use hiccup.core, hiccup.form-helpers, hiccup.page-helpers)
-)
+  (:use hiccup.core, hiccup.form-helpers, hiccup.page-helpers))
 
 ; Function to create html for a sidebar
 (defn display-sidebar []
   (html
     [:section {:class "sidebar"}
-      [:p [:a {:href "/post/new"} "New post"]]
-    ]
-  )
-)
+      [:p [:a {:href "/post/new"} "New post"]]]))
 
 ; Setup general page layout and include any js and css files we need
 (defn layout [title body]
   (html5
     [:head 
       [:title title]
-      (include-css "/css/blog.css")
-    ]
+      (include-css "/css/blog.css")]
     [:body
-      [:header
-        [:h1 
-          [:a {:href "/"} "Quick Blog"]
-        ]
-      ]
+      [:header [:h1 [:a {:href "/"} "Quick Blog"]]]
       (display-sidebar)
-      body
-    ] 
-  )
-)
+      body])) 
 
 ; Function to convert a post to html
 (defn html-post [post]
   (html
     [:div {:class "post"}
-      [:h2
-        [:a
-          {:href (str "/post/" (:id post))} 
-          (:title post)
-        ]
-      ]
+      [:h2 [:a {:href (str "/post/" (:id post))} (:title post)]]
       [:p (:body post)]
-      [:div {:class "date"} (:created_at post)]
-    ]
-  )
-)
+      [:div {:class "date"} (:created_at post)]]))
 
 ; Function to display a set of links to post actions
 (defn post-actions [id]
   [:div {:class "post-actions"}
     [:ul
       [:li
-        [:a 
-          {:href (str "/post/edit/" id)} 
-          "Edit Post"
-        ]
-      ]
+        [:a {:href (str "/post/edit/" id)} "Edit Post"]]
       [:li
-        [:a
-          {:href (str "/post/delete/" id)}
-          "Delete Post"
-        ]
-      ]
-    ]
-  ]
-)
+        [:a {:href (str "/post/delete/" id)} "Delete Post"]]]])
