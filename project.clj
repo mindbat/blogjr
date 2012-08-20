5e9bbf1ad4b650d6f22aa1c643c9d15b2148e46c
Ron Toland <rontoland@gmail.com>
2012-08-19 20:53:28 -0700

    Update to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,16 +1,13 @@
 (defproject blogjr "0.1.0-SNAPSHOT"
   :description "A simple, speedy blogging application written in clojure."
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
                  [org.clojure/java.jdbc "0.1.1"]
                  [compojure "0.6.4"]
                  [ring/ring-jetty-adapter "0.3.1"]
                  [hiccup "0.3.6"]
-                 [clj-time "0.3.0"]
-                ]
+                 [clj-time "0.3.0"]]
   :dev-dependencies [[lein-ring "0.4.5"]]
   :ring {:handler blogjr.core/app}
   :repositories {"central-proxy" 
-                 "http://repository.sonatype.org/content/repositories/central/"}
-)
+                 "http://repository.sonatype.org/content/repositories/central/"})
